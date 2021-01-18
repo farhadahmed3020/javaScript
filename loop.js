bf22cee26eee4cac9e613cbe1abffd64557639ac
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-18 17:18:46 +0600

    true vs false

--- loop.js
+++ loop.js
@@ -6,19 +6,19 @@
 // }
 // update
 
-// var number = 10 ;
-// while( number < 15){
-//     console.log(number);
-//     number++;
+var number = 10 ;
+while( number < 15){
+    console.log(number);
+    number++;
    
-// }
+}
 
-// //for loop//
-// for( var num =10;num< 15;num++){
-//     console.log(num);
-// }
+//for loop//
+for( var num =10;num< 15;num++){
+    console.log(num);
+}
 
-//  
+ 
 var nums =[11,22,33,44,55,66,77,88,99];
 for(var i =0;i<nums.length;i++){
     element=nums[i];
---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy value //
+//falsy
+// 0
+// ""
+//undefined
+//null
+//NaN
+// Truthy value //
+// " " ," 0 ",[]
+
+const name = 77;
+
+if(name || name == 0){
+    console.log(" condition is true ");
+}
+else{
+    console.log(" condition is false ");
+}
