9a861547868f26d717dbba6424d9d1c129327049
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-18 18:30:12 +0600

    double vs triple

---
+++ double-triple-equal.js
@@ -0,0 +1,32 @@
+
+// double equal 
+const firstNumber = 77 ;
+const secondNumber = "77";
+if(firstNumber == secondNumber){
+    console.log(" condition is true");
+}
+else{
+    console.log(" condition is false");
+}
+
+// javaScript roll (false = 0  and true = 1 )all time applicable //
+
+const first = 0 ;
+const second = false;
+if(first === second){
+    console.log(" condition is true");
+}
+else{
+    console.log(" condition is false");
+}
+
+
+ // triple equal //
+const number1 = 77 ;
+const number = "77";
+if(number === number1){
+    console.log(" condition is true");
+}
+else{
+    console.log(" condition is false");
+}
--- nudefined-null.js
+++ nudefined-null.js
@@ -1,16 +1,16 @@
-//let pakhi ;
+let pakhi ;
 
-// function add(num1 ,num2){
-//     const value = num1 + num2 ;
-//     return ;
-// }
+function add(num1 ,num2){
+    const value = num1 + num2 ;
+    return ;
+}
  
-// const result = add( 82 ,18);
-// console.log(result);
+const result = add( 82 ,18);
+console.log(result);
 
 
-// const lover = {name: "Salman Khan" ,phone: 905575, job: "actor"};
-// console.log(lover.gf);
+const lover = {name: "Salman Khan" ,phone: 905575, job: "actor"};
+console.log(lover.gf);
 
 const fun = undefined ;
 console.log(fun);
