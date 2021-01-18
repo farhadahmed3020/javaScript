bd7f6a4ffe5be8ea1290aa5d23d1255356bc2014
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-18 18:08:20 +0600

    null vs undefined

---
+++ nudefined-null.js
@@ -0,0 +1,19 @@
+//let pakhi ;
+
+// function add(num1 ,num2){
+//     const value = num1 + num2 ;
+//     return ;
+// }
+ 
+// const result = add( 82 ,18);
+// console.log(result);
+
+
+// const lover = {name: "Salman Khan" ,phone: 905575, job: "actor"};
+// console.log(lover.gf);
+
+const fun = undefined ;
+console.log(fun);
+
+let ages = [4 , 5 , 7 ]
+console.log(ages[5]);
