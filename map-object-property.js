c7b545ad800d8c18da38025e7b393d33530a5f70
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-19 10:17:27 +0600

    map done

---
+++ map-object-property.js
@@ -0,0 +1,20 @@
+const students =  [
+    {id: 21 ,name: 'Farhad Ahmed'},
+    {id: 32 ,name: 'Farhana Moni'},
+    {id: 43 ,name: 'Evan Ahmed'},
+    {id: 54 ,name: 'Fayshal Ahmed'}
+
+];
+const output = []
+
+for (let i = 0; i < students.length; i++ ){
+  const student = students[i] ;
+  const result = student.name ;
+  output.push(result);
+
+}
+console.log(output);
+
+// const names = students .map( s => s.name);
+//  const Ids = students.map ( s => s.id);
+//  console.log(Ids); 
